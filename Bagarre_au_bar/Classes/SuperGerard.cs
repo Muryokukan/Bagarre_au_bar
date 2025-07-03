0e2a4fbde97cc5b4525d8c3091cd833c70e962ae
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-07-03 15:38:27 +0200

    Create SuperGerard.cs

---
+++ Bagarre_au_bar/Classes/SuperGerard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class SuperGerard : LeGerard
+{
+    public SuperGerard() : base()
+    {
+        Nom = "Super Gerard"; // Change le nom
+        Console.WriteLine("Super Gerard (version améliorée)");
+    }
+
+    public override int Attaque()
+    {
+        int attaquebonus = random.Next(1, 10);
+        int attaquebasique = random.Next(2, 12);
+        int attaquetotel = attaquebasique + attaquebonus;
+
+        Console.WriteLine($"{Nom} lance une SUPER ATTAQUE et inflige {attaquebasique} dégâts !");
+
+        return attaquetotal;
+    }
+}
